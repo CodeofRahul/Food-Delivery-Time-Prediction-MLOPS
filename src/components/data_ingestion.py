ed5eddeeff0b8d8374587e455a4b8120c3abe976
CodeofRahul <420kumarahul@gmail.com>
2024-10-24 11:56:49 +0530

    Data Transformation Pipeline completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,4 +1,4 @@
-from src.config import *
+from src.constants import *
 from src.config.configuration import *
 import os, sys
 import pandas as pd
@@ -7,13 +7,13 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.logger import logging
 from src.exception import CustomException
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str = TRAIN_FILE_PATH
-    test_data_path: str = TEST_FILE_PATH
-    raw_data_path: str = RAW_FILE_PATH
-
+    train_data_path:str = TRAIN_FILE_PATH
+    test_data_path:str = TEST_FILE_PATH
+    raw_data_path:str = RAW_FILE_PATH
 
 
 class DataIngestion:
@@ -21,22 +21,22 @@ class DataIngestion:
         self.data_ingestion_config = DataIngestionConfig()
 
 
-    def initiate_data_ingestion(self):
+    def iniitiate_data_ingestion(self):
         try:
             df = pd.read_csv(DATASET_PATH)
 
-            # df = pd.read_csv(os.path.join("F:\ML_modular_coding_project\Machine_Learning_Modular_Coding_Project\Data\finalTrain.csv"))
-            
+            #df = pd.read_csv(os.path.join('C:\Users\shiva\Desktop\project_template\New-Machine-Learning-Modular-Coding-project\Data\finalTrain.csv'))
+
             os.makedirs(os.path.dirname(self.data_ingestion_config.raw_data_path), exist_ok=True)
 
-            df.to_csv(self.data_ingestion_config.raw_data_path, index=False)
+            df.to_csv(self.data_ingestion_config.raw_data_path, index = False)
 
-            train_set, test_set = train_test_split(df, test_size=0.20, random_state= 42)
+            train_set, test_set = train_test_split(df, test_size = 0.20, random_state= 42)
 
-            os.makedirs(os.path.dirname(self.data_ingestion_config.train_data_path), exist_ok=True)
+            os.makedirs(os.path.dirname(self.data_ingestion_config.train_data_path),exist_ok=True )
             train_set.to_csv(self.data_ingestion_config.train_data_path, header = True)
 
-            os.makedirs(os.path.dirname(self.data_ingestion_config.test_data_path),exist_ok=True)
+            os.makedirs(os.path.dirname(self.data_ingestion_config.test_data_path),exist_ok=True )
             test_set.to_csv(self.data_ingestion_config.test_data_path, header = True)
 
             return(
@@ -47,9 +47,17 @@ class DataIngestion:
 
 
         except Exception as e:
-            raise CustomException(e, sys)
-        
+            raise CustomException( e, sys)
+# Data Ingestion
+
+# if __name__ == "__main__":
+#    obj = DataIngestion()
+#    train_data, test_data = obj.iniitiate_data_ingestion()
+#    data_transformation = DataTransformation()
+#    train_arr, test_arr = data_transformation.inititate_data_transformation(train_data, test_data)
+
 if __name__ == "__main__":
     obj = DataIngestion()
-    train_data, test_data = obj.initiate_data_ingestion()
-        
+    train_data_path,test_data_path=obj.iniitiate_data_ingestion()
+    data_transformation = DataTransformation()
+    train_arr,test_arr,_ = data_transformation.inititate_data_transformation(train_data_path,test_data_path)
